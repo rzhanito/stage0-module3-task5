94330e3cf8b5b7201f144225e58872967f1c3738
Erzhan <95659303+rzhanito@users.noreply.github.com>
2023-05-13 02:01:18 +0600

    Update NumberDivider.java

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        float result = toBeDivided / 5;
+        double result = toBeDivided / 5;
         System.out.println(result);
     }
 }
